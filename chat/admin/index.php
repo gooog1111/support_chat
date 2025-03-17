373266ba2c9583ab8aeb46399c15116b03bcb5fe
gooog1111 <kiselev.ivan1111@gmail.com>
2025-03-17 06:40:57 +0300

    Add files via upload

--- chat/admin/index.php
+++ chat/admin/index.php
@@ -1,4 +1,10 @@
 <?php
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'curl') !== false || 
+    strpos($_SERVER['HTTP_USER_AGENT'], 'wget') !== false) {
+    // Перенаправляем curl и wget на альтернативный URL
+	header('HTTP/1.1 204 No Content');
+    exit;
+}
 header("Location: login.php");
 exit();
 ?>
